d2fe8b33f3c2d6e267d56624c0d99c5ac4289772
hefangqiang <15210500687@163.com>
2025-02-28 10:00:31 +0800

    二开，新增度空间和负载均衡

--- api/services/feature_service.py
+++ api/services/feature_service.py
@@ -76,7 +76,8 @@ class FeatureService:
 
         if dify_config.BILLING_ENABLED and tenant_id:
             cls._fulfill_params_from_billing_api(features, tenant_id)
-
+        # 二开新增
+        features.model_load_balancing_enabled = True
         return features
 
     @classmethod
@@ -98,7 +99,9 @@ class FeatureService:
         system_features.enable_email_password_login = dify_config.ENABLE_EMAIL_PASSWORD_LOGIN
         system_features.enable_social_oauth_login = dify_config.ENABLE_SOCIAL_OAUTH_LOGIN
         system_features.is_allow_register = dify_config.ALLOW_REGISTER
-        system_features.is_allow_create_workspace = dify_config.ALLOW_CREATE_WORKSPACE
+        # system_features.is_allow_create_workspace = dify_config.ALLOW_CREATE_WORKSPACE
+        # 二开修改
+        system_features.is_allow_create_workspace = True
         system_features.is_email_setup = dify_config.MAIL_TYPE is not None and dify_config.MAIL_TYPE != ""
 
     @classmethod
